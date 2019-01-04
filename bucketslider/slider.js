ffede240639103a510f8a54afa934d16cb6960bb
Juan Ramos <ptjuanramos@gmail.com>
2019-01-04 12:25:58 +0000

    Fixing old bugs and first attempt of spilling water animation

--- bucketslider/slider.js
+++ bucketslider/slider.js
@@ -3,6 +3,8 @@ const START_WATER_VALUE = 40;
 const LEAK_FILL_WATER_VALUE = 5;
 const LEAK_TIMER = 500; //milliseconds - less time more fast
 
+const WATER_ANIMATION_TIME = 100;
+
 const MUSIC_FILE_NAME = "music.mp3";
 
 var bucketWater;
@@ -40,18 +42,40 @@ $(document).ready(function() {
 
 function leakWater() {
     currentVolume = $("#volume").slider("value");
-    if(currentVolume >= 0) {
+    if(currentVolume > 0) {
+        spillWater();
+
         $("#volume").slider("value", currentVolume - LEAK_FILL_WATER_VALUE);
-        bucketWater.height(currentVolume - LEAK_FILL_WATER_VALUE + "%");
+
+        bucketWater.animate({
+            height: currentVolume - LEAK_FILL_WATER_VALUE + "%"
+        }, WATER_ANIMATION_TIME);
+
         myMusic.volume = currentVolume / 100;
     } 
 }
 
+//TODO - fix allot of shit
+// - maybe this is not the most correct way to do it
+function spillWater() {
+    $(".spillWater").animate({
+        width: "30px"  
+    }, WATER_ANIMATION_TIME * 3);
+
+    $(".spillWater").animate({
+        width: "0px"
+    }, WATER_ANIMATION_TIME);
+}
+
 function fillBucket() {
     currentVolume = $("#volume").slider("value");
-    if(currentVolume <= 100) {
+    if(currentVolume < 100) {
         $("#volume").slider("value", currentVolume + LEAK_FILL_WATER_VALUE);
-        bucketWater.height(currentVolume + LEAK_FILL_WATER_VALUE + "%");
+
+        bucketWater.animate({
+            height: currentVolume + LEAK_FILL_WATER_VALUE + "%"
+        }, WATER_ANIMATION_TIME);
+
         myMusic.volume = currentVolume / 100;
     } 
 }
